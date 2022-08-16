ff2d022e70ca85012d7ea45943b63f95d2e03833
rudyjm3 <rudyjm3@yahoo.com>
2022-08-16 13:57:27 -0400

    Added random computer choice function

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+const display = document.querySelector('#display');
 
-function playRound(playerSelection, computerSelection) {
-   // your code here!
+function getComputerChoice() {
+   //debugger
+   let randomNumber = Math.floor(Math.random() * 3);
+   switch (randomNumber) {
+      case 0:
+         return 'Rock'
+      case 1:
+         return 'Paper'
+      case 2:
+         return 'Scissors'
    }
+}
+//console.log(getComputerChoice());
+display.innerHTML = getComputerChoice();
+
+
+
+// function playRound(playerSelection, computerSelection) {
+//    // your code here!
+//    }
